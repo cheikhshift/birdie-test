630dbe7ca817da178797094acb4616f0680183fc
Cheikh Seck <jvmstix@gmail.com>
2022-06-05 16:03:27 +0200

    Update mood.ts
    - add endpoint to get total item count.

--- backend/src/controllers/mood.ts
+++ backend/src/controllers/mood.ts
@@ -4,6 +4,16 @@ import { BirdieDatabase } from '../db/mysql'
 export const moodController = express.Router();
 
 
+
+moodController.get('/count_events', async (_, res) => {
+  	const db = res.locals.db as BirdieDatabase
+  	var result = await db.query("SELECT COUNT(id) FROM events")
+
+  	res
+  	.status(200)
+  	.json({result : result[0]["COUNT(id)"]})
+}); 
+
 moodController.get('/events', async (req, res) => {
   	const db = res.locals.db as BirdieDatabase
   	const page = parseInt(
